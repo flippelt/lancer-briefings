ae739b51d8edd8c82ea21097d641d43aba09086b
Felipe Lippelt <lippelt.f@gmail.com>
2025-04-15 13:26:12 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: "./",
-  resolve: {
+@@ -10,5 +15,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  }
-})
+  },
+});
