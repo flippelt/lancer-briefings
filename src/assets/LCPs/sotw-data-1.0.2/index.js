2b991ee8091517da2b2c888ae52ef2d6d595d320
Felipe Lippelt <lippelt.f@gmail.com>
2025-07-09 19:36:08 -0300

    A lot of changes for tests

---
+++ src/assets/LCPs/sotw-data-1.0.2/index.js
@@ -0,0 +1,14 @@
+"use strict"
+import b from "./bonds.json"
+import f from "./frames.json"
+import p from "./pilot_gear.json"
+import s from "./systems.json"
+
+const data = {
+  bonds: b,
+  frames: f,
+  pilot_gear: p,
+  systems: s,
+}
+
+export default data;
